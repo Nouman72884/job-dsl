fc327439772a26535487b6d9bbbcefa938bfbf22
Nouman72884 <noumansatti7@gmail.com>
2020-06-24 22:46:53 +0500

    job dsl script

--- cloudformation.groovy
+++ cloudformation.groovy
@@ -1,21 +1,11 @@
 def gitUrl = 'git@github.com:Nouman72884/cloudformation-demo.git'
 
-def cloudFormationJobs =
-[
-    [ name: 'jenkins-cfn', templatePath: 's3cft.yaml', VpcName: 'demo-vpc.parameters', ec2Name: 'nouman-ec2.parameters' ]
-]
-
-cloudFormationJobs.each {
-
-    job('jenkins-cfn') {
-        scm {
-            git(gitUrl)
+job('jenkins-cfn') {
+    scm {
+        git(gitUrl)
         }
-        triggers {
-            scm('*/5 * * * *')
+    triggers {
+        scm('*/5 * * * *')
         }
-        steps {
-            shell("echo \"aws cloudformation --stack-name ${it['name']} --template-body file://${it['templatePath']} --parameters s3://${parametersBucket}/${it['parametersKeyName']}\" > ${it['name']}-cmd.txt")
-        }
+    
     }
-}
