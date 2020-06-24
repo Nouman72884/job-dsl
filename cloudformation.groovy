13a937a013c0f56b99ad9b66b6e0474c33614c41
Nouman72884 <noumansatti7@gmail.com>
2020-06-25 04:43:56 +0500

    job dsl script

--- cloudformation.groovy
+++ cloudformation.groovy
@@ -9,7 +9,7 @@ job('jenkins-cfn') {
     triggers {
         scm('*/5 * * * *')
         }
-    withAWS(region:AWS_REGION) {
+    withAWS(region:"${AWS_REGION}") {
         def response = cfnValidate(file:'s3cft.yaml')
         echo "template description: ${response.description}"
         def outputs = cfnUpdate(stack:'cfn-stack', file:'s3cft.yaml', params:['VpcName=demovpc','ec2Name=noumanec2'], timeoutInMinutes:10)
