508d8b0749402f75a3dc73995eeddb07e63f81a6
Nouman72884 <noumansatti7@gmail.com>
2020-06-24 22:53:48 +0500

    job dsl script

--- cloudformation.groovy
+++ cloudformation.groovy
@@ -1,11 +1,11 @@
-def gitUrl = 'git@github.com:Nouman72884/cloudformation-demo.git'
-
 job('jenkins-cfn') {
     scm {
-        git(gitUrl)
+        git('git://github.com/Nouman72884/cloudformation-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        node / gitConfigName('Nouman72884')
+        node / gitConfigEmail('noumansatti7@gmail.com')
         }
     triggers {
         scm('*/5 * * * *')
         }
-    
+
     }
