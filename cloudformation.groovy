6d09346d21423e22d927c358bacdafa4a4734bb0
Nouman72884 <noumansatti7@gmail.com>
2020-06-25 03:03:16 +0500

    job dsl script

--- cloudformation.groovy
+++ cloudformation.groovy
@@ -1,5 +1,4 @@
 def AWS_ROLE = "arn:aws:iam::020046395185:role/adminrole"
-def AWS_ID = "bfae2336-7b83-4046-8ef2-98f5b4c88975"
 def AWS_REGION = "us-east-1"
 job('jenkins-cfn') {
     scm {
@@ -10,7 +9,7 @@ job('jenkins-cfn') {
     triggers {
         scm('*/5 * * * *')
         }
-    withAWS(region: AWS_REGION, role: AWS_ROLE, credentials: AWS_ID) {
+    withAWS(region:AWS_REGION, role:AWS_ROLE) {
       def response = cfnValidate(file:'s3cft.yaml')
       echo "template description: ${response.description}"
 
