b303ee53d55de72abe6676826ad5a0b10f6b975b
Nouman72884 <noumansatti7@gmail.com>
2020-06-25 05:17:03 +0500

    job dsl script

--- cloudformation.groovy
+++ cloudformation.groovy
@@ -1,6 +1,6 @@
 def AWS_ROLE = "arn:aws:iam::020046395185:role/adminrole"
 def AWS_REGION = "us-east-1"
-pipeline('jenkins-cfn') {
+job('jenkins-cfn') {
     scm {
         git('git://github.com/Nouman72884/cloudformation-demo.git') {  node -> // is hudson.plugins.git.GitSCM
         node / gitConfigName('Nouman72884')
